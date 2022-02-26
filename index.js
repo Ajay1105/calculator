9ec3c1a4a9dfc77c6df6c0ce1bce393610b26510
Ajay <aky11052003@gmail.com>
2022-02-26 06:38:04 +0530

    supabase at line 19

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ document.querySelector("button").addEventListener("click", function () {
     var fn = parseInt(document.querySelector("#fn").value);
     var sn = parseInt(document.querySelector("#sn").value);
     var op = document.querySelector("#op").value;
-    document.querySelector("#ans").innerHTML = add(fn,sn,op);
+    document.querySelector("#ans").innerHTML = calculate(fn,sn,op);
 })
-function add(fn,sn,op){
+function calculate(fn,sn,op){
     if(op === "+"){
         return fn + sn ;
     }
